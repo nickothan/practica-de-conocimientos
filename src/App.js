868cb56f70aac51841ffccb82648f6ea1fd41680
Nickothan <s1nicolasrg@gmail.com>
2021-06-10 15:27:17 -0500

    Componente animaciones, archivos base y importacion en app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react'
+<<<<<<< HEAD
 
 import { Container } from './styles'
 import Router from './Router'
+=======
+import { Container, Description, Content } from './styles'
+
+import Textos from './components/Textos/Textos'
+import BackgroundSlice from './components/BackgroundSlide/BackgroundSlide'
+import IconsMenu from './components/IconsMenu/IconsMenu'
+import Botones from './components/Botones/Botones'
+import Animacion from './components/animaciones'
+>>>>>>> Componente animaciones, archivos base y importacion en app
 
 function App() {
   return (
     <Container>
+<<<<<<< HEAD
       <Router />
+=======
+      <Description>Practica de conocimientos</Description>
+      <Content>
+        <Animacion></Animacion>
+        <IconsMenu />
+        <Botones />
+        <BackgroundSlice />
+        <Textos />
+      </Content>
+>>>>>>> Componente animaciones, archivos base y importacion en app
     </Container>
   )
 }
---
+++ src/components/animaciones/Animaciones.jsx
@@ -0,0 +1,9 @@
+import { AnimacionesContainer } from './styles'
+
+export default function Animaciones() {
+  return (
+    <AnimacionesContainer>
+      <h1>Animaciones</h1>
+    </AnimacionesContainer>
+  )
+}
