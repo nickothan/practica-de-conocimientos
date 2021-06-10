8a0e1f5421543ac6942a8e27e12e20965f71e113
Nickothan <s1nicolasrg@gmail.com>
2021-06-10 17:31:28 -0500

    Organizando componentes en ruta

--- src/Pages/Estilos/Estilos.jsx
+++ src/Pages/Estilos/Estilos.jsx
@@ -1,7 +1,17 @@
+import Botones from '../../components/Botones'
+import Textos from '../../components/Textos'
+import IconsMenu from '../../components/IconsMenu'
+import BackgroundSlide from '../../components/BackgroundSlide'
+
 export default function Estilos() {
   return (
     <div>
-      <h1>Estilos</h1>
+      <h1>Estilos Basicos</h1>
+      <Botones />
+      <h1>Estilos Avanzados</h1>
+      <Textos />
+      <IconsMenu />
+      <BackgroundSlide />
     </div>
   )
 }
