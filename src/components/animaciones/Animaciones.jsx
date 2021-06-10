86ad0eb59a814c1c33bfbb5adfeb6c99231a198b
Nickothan <s1nicolasrg@gmail.com>
2021-06-10 18:05:02 -0500

    update

--- src/components/animaciones/Animaciones.jsx
+++
@@ -1,9 +0,0 @@
-import { AnimacionesContainer } from './styles'
-
-export default function Animaciones() {
-  return (
-    <AnimacionesContainer>
-      <h1>Animaciones</h1>
-    </AnimacionesContainer>
-  )
-}
