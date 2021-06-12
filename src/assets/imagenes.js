254d7b05d0dddbc29256f114a6c6a019ee732f89
Nickothan <s1nicolasrg@gmail.com>
2021-06-12 11:41:00 -0500

    Agregando y importando imagenes. Creando bd basica y importando a galeria interactiva

---
+++ src/assets/imagenes.js
@@ -0,0 +1,33 @@
+import img1 from './img/img1.jpg'
+import img2 from './img/img2.jpg'
+import img3 from './img/img3.jpg'
+import img4 from './img/img4.jpg'
+import img5 from './img/img5.jpg'
+import img6 from './img/img6.jpg'
+
+export const dataGaleria = [
+  {
+    img1: img1,
+    title: 'Book'
+  },
+  {
+    img2: img2,
+    title: 'tree'
+  },
+  {
+    img3: img3,
+    title: 'Bridge'
+  },
+  {
+    img4: img4,
+    title: 'Mountains'
+  },
+  {
+    img5: img5,
+    title: 'Elephant'
+  },
+  {
+    img6: img6,
+    title: 'Gigant'
+  }
+]
--- src/components/GaleriaInteractiva/GaleriaInteractiva.jsx
+++ src/components/GaleriaInteractiva/GaleriaInteractiva.jsx
@@ -3,11 +3,49 @@ import {
   GaleriaInteractivaContent
 } from './styles'
 
+import { dataGaleria } from '../../assets/imagenes'
+
 export default function GaleriaInteractiva() {
+  console.log('Data : ', dataGaleria)
   return (
     <GaleriaInteractivaContainer>
       <h3>Galeria Interactiva</h3>
-      <GaleriaInteractivaContent></GaleriaInteractivaContent>
+      <GaleriaInteractivaContent>
+        <ul>
+          <li>
+            <div>
+              <input type='radio' name='entrada' id='btn01' />
+              <label htmlFor='btn1'>
+                <img src='' alt='' />
+              </label>
+            </div>
+          </li>
+          <li>
+            <div>
+              <input type='radio' name='entrada' id='btn01' />
+              <label htmlFor='btn1'>
+                <img src='' alt='' />
+              </label>
+            </div>
+          </li>
+          <li>
+            <div>
+              <input type='radio' name='entrada' id='btn01' />
+              <label htmlFor='btn1'>
+                <img src='' alt='' />
+              </label>
+            </div>
+          </li>
+          <li>
+            <div>
+              <input type='radio' name='entrada' id='btn01' />
+              <label htmlFor='btn1'>
+                <img src='' alt='' />
+              </label>
+            </div>
+          </li>
+        </ul>
+      </GaleriaInteractivaContent>
     </GaleriaInteractivaContainer>
   )
 }
